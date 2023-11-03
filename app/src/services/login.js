98fdc19af02abeb2792b4feac33f1addf1a3283b
xenolito <oscar.rey.tajes@gmail.com>
2023-11-03 11:15:44 +0100

    First setup monorepo

--- app/src/services/login.js
+++ app/src/services/login.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const apiBaseUrl = 'http://localhost:4001/api/login'
+const apiBaseUrl = '/api/login'
 // const apiBaseUrl = 'https://evening-castle-33015-33fd40ba3c3e.herokuapp.com/api/login'
 
 const login = async (credentials) => {
--- app/vite.config.js
+++ app/vite.config.js
@@ -5,6 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    proxy: {
+      "/": {
+        target: "http://localhost:4001"
+      }
+    },
     port: 3000
   }
 })
